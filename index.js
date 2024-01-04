8a7be2295d285231b38d64ae87fce2480ada7c88
pohjus <jussi.pohjolainen@gmail.com>
2024-01-04 15:27:19 +0200

    feat: initial commit for RESTful api

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const { Sequelize, DataTypes } = require("sequelize");
+
+// Setting up a new Sequelize instance for SQLite database
+const sequelize = new Sequelize({
+  dialect: "sqlite",
+  storage: "./database.sqlite",
+});
+
+const Customer = sequelize.define(
+  "customer",
+  {
+    // Primary key, autoincremented
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    // Customer name, not null
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  { timestamps: false } // No timestamps
+);
+
+const app = express();
+const port = 3000;
+
+app.get("/api/customers", async (req, res) => {
+  const customers = await Customer.findAll();
+  res.send(customers);
+});
+
+app.listen(port, async () => {
+  console.log(`Example app listening on port ${port}`);
+
+  await sequelize.authenticate();
+  console.log("connection success");
+  await Customer.sync({ force: true });
+  await Customer.create({ name: "jack" });
+  await Customer.create({ name: "hannah" });
+  await Customer.create({ name: "tina" });
+});
